06a8636917f63997556dd546ec11ff04dba58bd3
rkpsx7 <rkpsx7@gmail.com>
2021-08-08 17:54:18 +0530

    completed

--- app/src/main/java/com/example/retrofit1/Network.java
+++ app/src/main/java/com/example/retrofit1/Network.java
@@ -7,11 +7,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Network {
 
-    private  static HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    private static HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
 
-    public static Retrofit getInstance(){
+    public static Retrofit getInstance() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://reqres.in/")
+                .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build())
                 .build();
